357a9ec186bf342dd93e0954e6509954c0dae197
Owen Evans <owen@3sidedcube.com>
2022-01-27 15:55:57 +0000

    decimal place can now be changed, with the default being 2

--- src/index.test.ts
+++ src/index.test.ts
@@ -34,6 +34,30 @@ describe("Temperature converter", () => {
   it("should return error message when given temperature isn't a number", async () => {
     const output = await exec("NaN");
 
-    expect(output[0]).toMatch(/^Error/);
+    expect(output[0]).toMatch(/^[Ee]rror/);
+  });
+
+  it("should return '104.63' when given temperature is '40.35' and -f option is set and -dp option is set to 3", async () => {
+    const output = await exec("40.35", "-f", "-dp", "3");
+
+    expect(output[0]).toBe("104.63");
+  });
+
+  it("should return '4.639' when given temperature is '40.35' and -c option is set and -dp option is set to 3", async () => {
+    const output = await exec("40.35", "-c", "-dp", "3");
+
+    expect(output[0]).toBe("4.639");
+  });
+
+  it("should return an error when no decimal place is given when the -dp option is present", async () => {
+    const output = await exec("40", "-dp");
+
+    expect(output[0]).toMatch(/^[Ee]rror/);
+  });
+
+  it("should return an error when the given decimal place option isn't a number", async () => {
+    const output = await exec("40", "-dp", "NaN");
+
+    expect(output[0]).toMatch(/^[Ee]rror/);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -11,27 +11,37 @@ const toFahrenheit: TempConvertor = f => f * (9 / 5) + 32;
 // subtract 32 and multiply by 5/9
 const toCelsius: TempConvertor = c => (c - 32) * (5 / 9);
 
-const round = (num: number): number => {
-  return +(Math.round(Number(num + "e+2")) + "e-2");
+const round = (num: number, dp: number = 2): number => {
+  return +(Math.round(Number(num + "e+" + dp)) + "e-" + dp);
 };
 
 program
   .version("0.0.1")
   .description("Temperature converter CLI Tool - by Owen Evans")
-  .argument("<temperature>", "temperature to convert")
+  .argument("<temperature>", "temperature to convert, converts to fahrenheit by default")
   .option("-c, --celsius", "convert temperature to celsius")
   .option("-f, --fahrenheit", "convert temperature to fahrenheit")
-  .action((temperature: string, options: { fahrenheit: boolean; celsius: boolean }) => {
+  .option(
+    "-dp, --decimalPlace <dp>",
+    "number of decimal place the returned temperature will be rounded to, default is 2dp"
+  )
+  .action((temperature: string, options: { fahrenheit: boolean; celsius: boolean; decimalPlace: string }) => {
     let temp = parseFloat(temperature);
+    let dp = options.decimalPlace ? parseInt(options.decimalPlace) : 2;
 
     if (Number.isNaN(temp)) {
       console.error(new Error("<temperature> needs to be a number"));
       return;
     }
 
+    if (Number.isNaN(dp)) {
+      console.error(new Error("-dp <dp> needs to be a number"));
+      return;
+    }
+
     temp = options.celsius ? toCelsius(temp) : toFahrenheit(temp);
 
-    temp = round(temp);
+    temp = round(temp, dp);
 
     console.log(temp);
   });
